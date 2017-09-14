acb51aa268c827ec218db65aa7eda9bb7b73636f
David Parish <dparish@gmail.com>
2017-09-14 10:47:06 -0500

    added frontend

--- backend/src/main/java/dparish/starter/rest/IndexController.java
+++ backend/src/main/java/dparish/starter/rest/IndexController.java
@@ -1,13 +1,14 @@
 package dparish.starter.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
 
     @GetMapping("/")
     public String showIndexPage() {
-        return "hi";
+        return "forward:/index.html";
     }
 }
